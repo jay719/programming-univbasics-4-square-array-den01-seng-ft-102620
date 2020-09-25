f25d0a52848769b0955dd36f375b57e489176c75
Javaria Brascom <javariab17@gmail.com>
2020-09-25 02:23:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,14 @@ def square_array(array)
   new_array=[]
   counter = 0
   while counter < array.length do
+<<<<<<< HEAD
      
- new_array.push(array[counter]**2)
+ new_array.push(array**2)
 counter += 1
 end
 return new_array
+=======
+   new_array.push(i**2)
+end
+>>>>>>> 65fe212a2591f2949c771fffc1218e7ef8b2aaeb
 end
